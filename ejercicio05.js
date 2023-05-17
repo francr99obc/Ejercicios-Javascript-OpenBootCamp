56c17b3fb7fe49cc787019ac34c268d4a3fa5e5f
francr99obc <131280763+francr99obc@users.noreply.github.com>
2023-05-17 18:03:21 +0200

    Add files via upload

---
+++ ejercicio05.js
@@ -0,0 +1,18 @@
+// Ejercicio
+let altura = 170;
+console.log(altura);
+// ...
+let altura_m = 1.70;
+console.log(altura_m);
+// ...
+let peso = 69.5;
+console.log(peso);
+// ...
+let altura_r = 1.70;
+console.log(altura_r.toFixed(0));
+// ...  
+let peso_r = 69.5;
+console.log(peso_r.toFixed(0));
+// ...
+let maxNumberPlusOneEqualsMax = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(maxNumberPlusOneEqualsMax);
